0d0774c4aea59651a3101022b422e60a5f3c4f49
DEVIPRIYA <dpdevipriya23@gmail.com>
2020-02-24 21:35:02 +0530

    first commit

---
+++ App.java
@@ -0,0 +1,13 @@
+package com.vnr.devipriya.calculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ MathOperation.java
@@ -0,0 +1,26 @@
+package com.vnr.devipriya.calculator;
+
+
+public class MathOperation
+{
+int add(int m,int n)
+{
+int sum = m + n;
+return(sum);
+}
+int sub(int m,int n)
+{
+int subans = m - n;
+return(subans);
+}
+int mul(int m,int n)
+{
+int mulans = m * n;
+return(mulans);
+}
+double div(double m, double n)
+{
+double divans = (double)m / n;
+return(divans);
+}
+}
